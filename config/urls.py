62c765f098082b0802b6b2adb96ae768e262732e
Samuel <samuelonyeibor21@gmail.com>
2025-12-21 18:31:54 +0100

    update urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path('api/user/', include('apps.accounts.user_urls')),
 ]
 
-if True:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
